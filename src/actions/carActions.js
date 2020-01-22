ba4eb2c96b38f232188a338cf711bf318ce17b4f
christina kaliora <develop@pirron.one>
2020-01-22 23:32:15 +0100

    finished MVP ..... dunno how

---
+++ src/actions/carActions.js
@@ -0,0 +1,7 @@
+export const addItem = (payload) => {
+    return { type: "ADD_ITEM", payload };
+}
+
+export const removeItem = (payload) => {
+    return { type: "REMOVE_ITEM", payload };
+}
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -2,17 +2,18 @@ import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
+  
   return (
-    <div className="content">
+    <div className="content"> 
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addItem={props.addItem}/>
           ))}
         </ol>
       ) : (
-        <p>Nice looking car!</p>
+        <p>Nice looking car!</p> 
       )}
     </div>
   );
